1e7b254df19c39f6eac0df13f3befbf194c531e7
ypq123456789 <114487221+ypq123456789@users.noreply.github.com>
2024-06-27 13:33:22 +0800

    Update rclone_sync.sh

--- rclone_sync.sh
+++ rclone_sync.sh
@@ -7,16 +7,8 @@ COMMAND_FILE="/root/rclone_command.txt"
 
 echo "---------------------------------------------" >> $LOG_FILE
 
-# 检查是否存在定时任务
-if ! crontab -l | grep -q "rclone_sync.sh"; then
-    # 添加新的定时任务
-    echo "未发现定时任务，正在添加新的定时任务..."
-    (crontab -l ; echo "0 * * * * cd /root && ./rclone_sync.sh") | crontab -
-    echo "$(date) 添加新的定时任务：0 * * * * cd /root && ./rclone_sync.sh" >> $LOG_FILE
-else
-    echo "已存在定时任务。"
-    echo "$(date) 已存在定时任务。" >> $LOG_FILE
-fi
+# 记录脚本执行开始时间
+start_time=$(date +%s)
 
 # 检查是否安装了rclone，若未安装则进行安装
 if ! command -v rclone &> /dev/null; then
@@ -24,7 +16,7 @@ if ! command -v rclone &> /dev/null; then
     echo "$(date) rclone 未找到，正在安装..." >> $LOG_FILE
     sudo apt install rclone -y
     # 询问是否更换rclone二进制文件
-    echo "是否需要更换rclone的二进制文件？请在10s内输入直链网址，否则按回车继续。"
+    echo "是否需要更换rclone的二进制文件？请在10s内输入直链网址，否则按回车继续."
     read -t 10 binary_link
     if [ ! -z "$binary_link" ]; then
         echo "正在从 $binary_link 下载rclone并安装到 /usr/bin/rclone 下..."
@@ -32,23 +24,23 @@ if ! command -v rclone &> /dev/null; then
         echo "$(date) 从 $binary_link 下载并安装rclone到 /usr/bin/rclone。" >> $LOG_FILE
     fi
 else
-    echo "rclone已经安装。"
-    echo "$(date) rclone已经安装。" >> $LOG_FILE
+    echo "rclone已经安装."
+    echo "$(date) rclone已经安装." >> $LOG_FILE
 fi
 
 # 检查rclone配置文件
 echo "$(date) 检查rclone配置文件" >> $LOG_FILE
 config_file="/root/.config/rclone/rclone.conf"
 if [ -f "$config_file" ];then
-    echo "rclone 配置文件已存在。"
-    echo "$(date) rclone 配置文件已存在。" >> $LOG_FILE
+    echo "rclone 配置文件已存在."
+    echo "$(date) rclone 配置文件已存在." >> $LOG_FILE
 else
     echo "rclone 配置文件不存在，请提供配置文件的直链URL："
     read config_link
     if [ ! -z "$config_link" ];then
         mkdir -p $(dirname "$config_file") && curl -L $config_link -o "$config_file"
-        echo "已从 $config_link 下载rclone配置文件到 $config_file。"
-        echo "$(date) 已从 $config_link 下载rclone配置文件到 $config_file。" >> $LOG_FILE
+        echo "已从 $config_link 下载rclone配置文件到 $config_file."
+        echo "$(date) 已从 $config_link 下载rclone配置文件到 $config_file." >> $LOG_FILE
     fi
 fi
 
@@ -66,8 +58,8 @@ if [ ! -f $COMMAND_FILE ]; then
     # 读取用户输入的新参数
     read_command_params
 else
-    echo "使用保存的 rclone sync 命令参数。"
-    echo "$(date) 使用保存的 rclone sync 命令参数。" >> $LOG_FILE
+    echo "使用保存的 rclone sync 命令参数."
+    echo "$(date) 使用保存的 rclone sync 命令参数." >> $LOG_FILE
     command_params=$(cat $COMMAND_FILE)
     echo "当前参数为：$command_params"
     echo "如果您想要修改这些参数，请在10s内输入 'y'："
@@ -77,8 +69,8 @@ else
         echo "$(date) 用户选择修改参数" >> $LOG_FILE
         read_command_params
     else
-        echo "没有修改rclone sync命令。"
-        echo "$(date) 没有修改rclone sync命令。" >> $LOG_FILE
+        echo "没有修改rclone sync命令."
+        echo "$(date) 没有修改rclone sync命令." >> $LOG_FILE
     fi
 fi
 
@@ -86,6 +78,17 @@ fi
 echo "$(date) 执行 rclone sync 命令" >> $LOG_FILE
 bash -c "$(cat $COMMAND_FILE)"
 
-echo "rclone sync 任务已完成。"
-echo "$(date) rclone sync 任务已完成。" >> $LOG_FILE
+# 记录脚本执行结束时间
+end_time=$(date +%s)
+
+# 计算脚本执行时间
+execution_time=$((end_time - start_time))
+
+# 输出执行时间到日志文件
+echo "脚本执行开始时间：$(date -d @$start_time)" >> $LOG_FILE
+echo "脚本执行结束时间：$(date -d @$end_time)" >> $LOG_FILE
+echo "脚本执行共用时：$execution_time 秒" >> $LOG_FILE
+
+echo "rclone sync 任务已完成."
+echo "$(date) rclone sync 任务已完成." >> $LOG_FILE
 echo "---------------------------------------------" >> $LOG_FILE
